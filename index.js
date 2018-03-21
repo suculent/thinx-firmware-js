4e02ed8ed87d7ab1e556108d19f7763f0c5eb57c
Matěj Sychra <suculent@me.com>
2018-03-22 00:16:35 +0100

    removed runloop, held by reboot timer

--- index.js
+++ index.js
@@ -20,11 +20,4 @@ var rollbar = new Rollbar({
 var thinx = require('./lib/thinx/thinx.js');
 thinx.init(defaults.thinx.api_key, defaults.thinx.owner);
 
-var runloopTimer = setInterval(function(){ runloop(); }, 60000);
-
-function runloop() {
-    thinx.checkin();
-    console.log(new Date().toString());
-}
-
 Rollbar.info("thinx-lib-js started", {postId: 123});
