dc218afcc1a0b6de69b885acf635108ead0f6a1a
Matěj Sychra <suculent@me.com>
2018-03-22 13:46:36 +0100

    removed development dependencies, fixed issue with potentially undefined callback(s)

--- test.js
+++ test.js
@@ -5,23 +5,9 @@
 var defaults = require("./conf/config.json");
 console.log("Initializing remote log...");
 
-var Rollbar = require("rollbar");
-var rollbar = new Rollbar({
-  accessToken: defaults.rollbar.client_token,
-  handleUncaughtExceptions: false,
-  handleUnhandledRejections: false,
-  captureUncaught: false,
-  captureUnhandledRejections: false,
-  payload: {
-    environment: "development"
-  }
-});
-
 var thinx = require('./lib/thinx/thinx.js');
 thinx.init(defaults.thinx.api_key, defaults.thinx.owner);
 thinx.setCheckinCallback(function(){
   console.log("Test completed, exiting...");
   process.exit();
 });
-
-Rollbar.info("thinx-lib-js started", {postId: 123});
