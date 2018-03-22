745270f7555f6a8e32f1c3c16ff2bcbd9d84bc9b
Matěj Sychra <suculent@me.com>
2018-03-22 11:55:49 +0100

    removed useless code from index.js; does not work anyway for some reason

---
+++ test.js
@@ -0,0 +1,23 @@
+/*
+ * JavaScript client library for Remote Things Management using THiNX platform.
+ */
+
+var defaults = require("./conf/config.json");
+console.log("Initializing remote log...");
+
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: defaults.rollbar.client_token,
+  handleUncaughtExceptions: false,
+  handleUnhandledRejections: false,
+  captureUncaught: false,
+  captureUnhandledRejections: false,
+  payload: {
+    environment: "development"
+  }
+});
+
+var thinx = require('./lib/thinx/thinx.js');
+thinx.init(defaults.thinx.api_key, defaults.thinx.owner);
+
+Rollbar.info("thinx-lib-js started", {postId: 123});
