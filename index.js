f69c630542a8a3cb0aa066f4ee85fd2a319c01a4
Mikuláš Sychra <mikulas.sychra@gmail.com>
2018-03-22 11:33:10 +0100

    test

--- index.js
+++ index.js
@@ -18,13 +18,14 @@ var rollbar = new Rollbar({
 });
 
 var thinx = require('./lib/thinx/thinx.js');
-thinx.init(defaults.thinx.api_key, defaults.thinx.owner);
-
-Rollbar.info("thinx-lib-js started", {postId: 123});
 
 /*
-  Custom callbacks
+  Custom settings START
 */
+
+thinx.setCheckinInterval(10);
+thinx.setLocation(51,14);
+thinx.setStatus("[SLACKME]");
 
 thinx.setMQTTCallback(function(message) {
   // incoming mqtt message
@@ -37,3 +38,11 @@ thinx.setPushConfigCallback(function(configuration) {
 thinx.setCheckinCallback(function() {
   // checkin completed
 });
+
+/*
+  Custom settings END
+*/
+
+thinx.init(defaults.thinx.api_key, defaults.thinx.owner);
+
+Rollbar.info("thinx-lib-js started", {postId: 123});
